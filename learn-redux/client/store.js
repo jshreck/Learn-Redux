d93da849a312bed01d3ea431ddb4e6eb7c08011f
Julia Shreckhis <jshreckhis@RICJSHRECKHISE.local>
2018-09-21 17:17:48 -0400

    completed video 6, actions created

---
+++ learn-redux/client/store.js
@@ -0,0 +1,18 @@
+import { createStore, compse } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+import rootreducer from './reducers/index';
+
+import comments from './data/comments';
+
+const defaultState = {
+    posts, 
+    comments
+};
+
+const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory), store;
+
+export default store;
